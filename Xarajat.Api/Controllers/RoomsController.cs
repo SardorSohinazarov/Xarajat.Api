1f7b76f9c45ea07135229656c98bdd942b03114e
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-04 08:40:28 +0500

    Room Model added

---
+++ Xarajat.Api/Controllers/RoomsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Xarajat.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoomsController : ControllerBase
+{
+
+}
